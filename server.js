392c479f7dde5d37fe4a887d7dc18bdc11f2fb3f
Umair Amjad <umairamjad970@gmail.com>
2026-04-25 22:45:38 +0530

    Fix deployment port issue

--- server.js
+++ server.js
@@ -17,24 +17,6 @@ export const io = new Server(server, {
   cors: { origin: "*" }
 })
 
-//Store online users 
-// export const userSocketMap = {};  // { userId: socketId }
-
-// // Socket.IO connection handling
-// io.on("connection", (socket) => {
-//   const userId = socket.handshake.query.userId;
-//   console.log("User Connected", userId);
-//   if (userId) userSocketMap[userId] = socket.id;
-
-//   //Emit online users to all clients
-//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", userId);
-//     delete userSocketMap[userId];
-//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//   });
-
-// });
 
 // Store online users 
 export const userSocketMap = {};
@@ -100,7 +82,13 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 // Start server
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
